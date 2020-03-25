1c90f5ddf4088e0d5a0960e38b1c3ee9c79da826
Ali Yamini <mail@aliyamini.ir>
2020-03-26 01:51:43 +0430

    Add authentication for user

--- back-end/alnk_project/settings.py
+++ back-end/alnk_project/settings.py
@@ -139,7 +139,7 @@ CACHES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
