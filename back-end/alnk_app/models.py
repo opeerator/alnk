8a8c815dde78cdd15ff65f7906a66fd29c1fdd90
Ali Yamini <mail@aliyamini.ir>
2020-03-26 01:10:15 +0430

    Adding link model

--- back-end/alnk_app/models.py
+++ back-end/alnk_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Link(models.Model):
+    owner = models.ForeignKey(User, unique=False, on_delete=models.CASCADE)
+    original_link = models.URLField(null=False)
+    shortened_link = models.URLField(null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
