797e399e7f0da3d67a6b32b5098b3f4972c13023
Ali Yamini <mail@aliyamini.ir>
2020-03-31 15:16:57 +0430

    Adding user serializer

--- back-end/alnk_app/serializers.py
+++ back-end/alnk_app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 from .models import Link
 
@@ -6,4 +7,13 @@ from .models import Link
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ('id', 'owner', 'original_link', 'shortened_link' , 'date_created')
+        fields = ('id', 'owner', 'original_link', 'shortened_link',
+                 'date_created')
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
