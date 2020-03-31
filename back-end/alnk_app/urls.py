093531c0a25abe2f402f655f4c1889ca3aa880e0
Ali Yamini <mail@aliyamini.ir>
2020-03-31 15:27:17 +0430

    adding urls for users list and detail

--- back-end/alnk_app/urls.py
+++ back-end/alnk_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-
-
-from .views import ListLink, DetailLink
+from .views import ListLink, DetailLink, Userlist, UserDetail
 
 
 urlpatterns = [
     path('<int:pk>/', DetailLink.as_view()),
     path('', ListLink.as_view()),
+    path('users/', Userlist.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view())
 ]
